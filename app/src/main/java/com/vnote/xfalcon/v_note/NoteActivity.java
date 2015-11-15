01c9c83ed34b1645f232f4a52d847b7e4316dae2
MDAMI <mahadamir@hotmail.com>
2015-11-15 05:32:30 -0500

    Initial Code Base

---
+++ app/src/main/java/com/vnote/xfalcon/v_note/NoteActivity.java
@@ -0,0 +1,16 @@
+package com.vnote.xfalcon.v_note;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mahad on 2015-11-15.
+ */
+public class NoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+    }
+}
