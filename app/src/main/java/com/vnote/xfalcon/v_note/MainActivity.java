bc23766885f1da6bf4f4e762d7ab5090959e21e1
Wesley <14wbk@queensu.ca>
2015-11-19 16:07:32 -0500

    Added onClick elements to buttons in activity_main to other activities

--- app/src/main/java/com/vnote/xfalcon/v_note/MainActivity.java
+++ app/src/main/java/com/vnote/xfalcon/v_note/MainActivity.java
@@ -1,7 +1,11 @@
 package com.vnote.xfalcon.v_note;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void addButtonPressed(View view) {
+        Intent intent = new Intent(this, NoteActivity.class);
+        startActivity(intent);
+
+    }
+    public void recordButtonPressed(View view) {
+        Intent intent = new Intent(this, RecordActivity.class);
+        startActivity(intent);
+
+    }
+    public void searchButtonPressed(View view) {
+        Intent intent = new Intent(this, null);
+        startActivity(intent);
+
     }
 }
