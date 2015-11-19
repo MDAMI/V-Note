de8a4707cde1e057e59503b0ae0823e47e7acc88
Wesley <14wbk@queensu.ca>
2015-11-19 16:30:15 -0500

    Back button onclick in record and note activities

--- app/src/main/java/com/vnote/xfalcon/v_note/RecordActivity.java
+++ app/src/main/java/com/vnote/xfalcon/v_note/RecordActivity.java
@@ -1,7 +1,9 @@
 package com.vnote.xfalcon.v_note;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RecordActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class RecordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+    }
+
+    public void goHome(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
