9ff7a17ef6f37758fa186c39b76c59fed1fa2662
Wesley <14wbk@queensu.ca>
2015-11-26 16:13:04 -0500

    maybe voice

--- app/src/main/java/com/vnote/xfalcon/v_note/RecordActivity.java
+++ app/src/main/java/com/vnote/xfalcon/v_note/RecordActivity.java
@@ -1,27 +1,35 @@
 package com.vnote.xfalcon.v_note;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Environment;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.TextView;
 
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RecordActivity extends AppCompatActivity {
 
     private MediaPlayer mPlayer;
     private MediaRecorder recorder;
     private String OUTPUT_FILE;
-    Chronometer chronometer;
-    Button stop;
-
+    private Chronometer chronometer;
+    private Button stop;
+    private TextView txtText;
+    protected static final int RESULT_SPEECH = 1;
 
     //private File[] files;
 
@@ -30,8 +38,28 @@ public class RecordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
         OUTPUT_FILE = Environment.getExternalStorageDirectory()+"/recorder.3gpp";
+
+        txtText = (TextView) findViewById(R.id.txtText);
+
+
         try {
             beginRecording();
+
+
+
+            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+
+            try {
+                startActivityForResult(intent, RESULT_SPEECH);
+                txtText.setText("");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -62,6 +90,26 @@ public class RecordActivity extends AppCompatActivity {
                 break;
         }
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case RESULT_SPEECH: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> text = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                    txtText.setText(text.get(0));
+                }
+                break;
+            }
+
+        }
+    }
+
+
     private void beginRecording() throws Exception{
         ditchMediaRecorder();
         File outFile = new File(OUTPUT_FILE);
